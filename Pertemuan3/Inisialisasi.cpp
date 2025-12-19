62cffa156ab9d8f049551ab79c43fc671531e863
AnnisaPartiwi <annisapartiwi.2912@gmail.com>
2025-12-19 17:27:57 +0700

    Add files via upload

---
+++ Pertemuan3/Inisialisasi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool b = true;             // inisialisasi variabel b dengan nilai kebenaran "true"
+    int i = 6;                 // inisialisasi variabel i dengan nilai bilangan bulat 6
+    float f = 3.25;            // inisialisasi variabel f dengan nilai desimal 3.25
+    double d = 5.1894;       // inisialisasi variabel d dengan nilai desimal 5.1894
+    char c = 'B';              // inisialisasi variabel c dengan karakter B
+    string s = "Hello World"; // inisialisasi variabel s dengan kata Hello World
+    return 0;
+}
