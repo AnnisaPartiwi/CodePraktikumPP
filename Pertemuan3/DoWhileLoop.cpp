624d0790c2df7bab84fed3a6a216a49a48bebac9
AnnisaPartiwi <annisapartiwi.2912@gmail.com>
2025-12-19 17:22:49 +0700

    Add files via upload

---
+++ Pertemuan3/DoWhileLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int kalimat = 1;
+    do {
+        cout << "Saya Cinta Banget C++" << endl;
+        kalimat++;
+    } while (kalimat <= 5);
+    return 0;
+}
---
+++ Pertemuan3/NestedLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int baris = 5;
+    int kolom = 6;
+
+    for(int i = 1; i <= baris; i++){
+        for(int j = 1; j <= kolom; j++){
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
